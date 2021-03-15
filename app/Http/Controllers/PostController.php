23ba1a2d8a52b2ff15213d09676ca2b3e3f6dba9
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-15 13:23:38 +0700

    laravel restapi

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $data = Post::all();
+        $data = Post::paginate(5);
         return new PostCollection($data);
     }
 
