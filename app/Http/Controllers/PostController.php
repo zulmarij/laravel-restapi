021a9a61b1228e2c632e48e62f91544b66d723ee
Muhammad Zul Marij Rizqi Fathullah <muhammadzulmarijrizqifathullah@gmail.com>
2021-03-15 10:21:03 +0700

    laravel restapi

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\Post\PostResource;
+use App\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $data = Post::all();
+        return response()->json($data, 200);
+    }
+
+    public function show($id)
+    {
+        $data = Post::find($id);
+
+        if (is_null($data)) {
+            return response()->json([
+                'message' => 'Resource not found!'
+            ], 404);
+        }
+
+        return new PostResource($data);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $validator = Validator::make($data, [
+            'title' => 'required|min:5`'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'error' => $validator->errors()
+            ], 400);
+        }
+
+        $response = Post::create($data);
+        return response()->json($response, 201);
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $post->update($request->all());
+        return response()->json($post, 200);
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json(null, 200);
+    }
+}
---
+++ app/Http/Resources/Post/PostResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Post;
+
+use App\Http\Resources\User\UserResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'title' => $this->title,
+            'body' => $this->body,
+            'stored_at' => $this->created_at->diffForHumans(),
+            // 'user' => $this->user
+            // 'user' => new UserResource($this->user),
+            // 'comments' => $this->comments
+        ];
+    }
+}
